99beaebb6f3fbdee4198f46e95027bcffd1ce2e3
pointseven1 <m.gaafer@pointseven.net>
2024-12-15 09:55:38 +0200

    [FIX]

--- stock_history_report/__manifest__.py
+++ stock_history_report/__manifest__.py
@@ -1,13 +1,14 @@
 {
     "name": "Stock History Report",
-    "summary": "Export stock history for specified datetime ranges across one or multiple warehouses or locations.",
+    "summary": "Export XLSX report stock history for specified datetime ranges across one or multiple warehouses or locations."
+               "You can search by warehouses / locations / sub locations / Supplier / Category ",
     "version": "16.0",
     "category": "Warehouse/Reports",
     "author": "Mahmoud Gaafer",
     "maintainer": "Mahmoud Gaafer",
     "website": "https://www.linkedin.com/in/mahmoud-gaafer-b530191a9",
     "license": "LGPL-3",
-    "price": 4,
+    "price": 20,
     "currency": "USD",
     "depends": ["stock", "base"],
     "data": [
